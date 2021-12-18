319fb377252b11f5bcaa67a975ac7cd197fbaf7e
Mata <matija.stampfel@gmail.com>
2021-12-18 18:28:26 +0300

    bootstrap removed, only css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,5 @@
 import React, { useState } from "react";
 import "./App.css";
-import {
-  Alert,
-  Button,
-  Col,
-  Container,
-  Form,
-  OverlayTrigger,
-  Row,
-  Tooltip,
-} from "react-bootstrap";
 
 function App() {
   const [input, setInput] = useState("");
@@ -26,86 +16,84 @@ function App() {
 
   return (
     <div className="App">
-      <Container className="form-container">
-        <Row className="around">
-          <Col>
-            <Form
-              onSubmit={(e) => {
-                logIn();
-                e.preventDefault();
+      <form
+        onSubmit={(e) => {
+          logIn();
+          e.preventDefault();
+        }}
+        className="form-container"
+      >
+        <h1 className="login">Название веб-сайта</h1>
+        <div className="border-line">
+          <div className="control">
+            <label>Логин</label>
+
+            <input
+              type="text"
+              placeholder="введите электронной почты"
+              value={input}
+              onChange={(e) => {
+                setInput(e.target.value);
               }}
+            />
+          </div>
+
+          <div className="control">
+            <label>Пароль</label>
+            <input
+              type="password"
+              placeholder="Пароль"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              id="checked"
+              checked={checkbox}
+              onChange={(e) => {
+                setCheckbox(e.target.checked);
+              }}
+            />
+            <label htmlFor="checked">согласен с офертой</label>
+          </div>
+          <span className="actions">
+            <button
+              className="hovertext"
+              data-hover={
+                input.length === 0 ||
+                password.length === 0 ||
+                checkbox === false
+                  ? "Пожалуйста, заполните все поля"
+                  : "все поля заполнены"
+              }
+              type="submit"
+              disabled={
+                input.length === 0 ||
+                password.length === 0 ||
+                checkbox === false
+                  ? true
+                  : false
+              }
             >
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Логин</Form.Label>
-
-                <Form.Control
-                  type="text"
-                  placeholder="введите электронной почты"
-                  value={input}
-                  onChange={(e) => {
-                    setInput(e.target.value);
-                  }}
-                />
-              </Form.Group>
-
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Пароль</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Пароль"
-                  value={password}
-                  onChange={(e) => {
-                    setPassword(e.target.value);
-                  }}
-                />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check
-                  type="checkbox"
-                  label="согласен с офертой"
-                  checked={checkbox}
-                  onChange={(e) => {
-                    setCheckbox(e.target.checked);
-                  }}
-                />
-              </Form.Group>
-              <OverlayTrigger
-                show={!sucessfullLogIn}
-                placement="right"
-                trigger="hover"
-                overlay={
-                  <Tooltip id="tooltip-disabled">
-                    Пожалуйста, введите логин и пароль
-                  </Tooltip>
-                }
-              >
-                <span className="d-inline-block">
-                  <Button
-                    variant={sucessfullLogIn ? "btn btn-success" : "primary"}
-                    type="submit"
-                    disabled={
-                      input.length === 0 ||
-                      password.length === 0 ||
-                      checkbox === false
-                        ? true
-                        : false
-                    }
-                  >
-                    Войти
-                  </Button>
-                </span>
-              </OverlayTrigger>
-            </Form>
-            {sucessfullLogIn && (
-              <div>
-                <Alert variant="success">
-                  <Alert.Heading>You are Login!</Alert.Heading>
-                </Alert>
-              </div>
-            )}
-          </Col>
-        </Row>
-      </Container>
+              Войти
+            </button>
+          </span>
+        </div>
+        {sucessfullLogIn && (
+          <div>
+            <h2>
+              You are log in !{" "}
+              <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                Check this ultra secret link
+              </a>{" "}
+            </h2>
+          </div>
+        )}
+      </form>
     </div>
   );
 }
